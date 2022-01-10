6b135c5b7bbdbdd5848e89385ac5dd27c1dc1fa6
mastiaha <mastiaha@mail.ru>
2022-01-10 19:58:37 +0300

    Решение задачи 2

---
+++ 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = String.Empty;
+            using (StreamReader sr = new StreamReader("../../../Products.json"))
+            {
+                jsonString = sr.ReadToEnd();
+            }
+            Tovar[] tovari = JsonSerializer.Deserialize<Tovar[]>(jsonString);
+            Tovar maxTovar = tovari[0];
+            foreach (Tovar s in tovari)
+            {
+                if (s.Cena>maxTovar.Cena)
+                {
+                    maxTovar = s;
+                }
+            }
+            Console.WriteLine("Самый дорогой товар {0}. Код самого дорогого товара {1}. Стоимость {2} рублей.", maxTovar.Name, maxTovar.Kod, maxTovar.Cena);
+            Console.WriteLine("Для вывхода из программы нажмите любую клавишу");
+            Console.ReadKey();
+    }
+    }
+}
---
+++ 2/Tovar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Tovar
+    {
+        public int Kod { get; set; }
+        public string Name { get; set; }
+        public double Cena { get; set; }
+    }
+}
